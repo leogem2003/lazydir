487e39632a7f565684c632e17c63f8d4432e6526
Leonardo Gementi <leogem05@gmail.com>
2022-12-24 16:12:52 +0100

    changed project name to lazydir in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fileconsole',
+    name='lazydir',
     version='0.1.0',
     py_modules=find_packages(),
     install_requires=[
@@ -9,7 +9,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'fileconsole = fileconsole.cli:cli',
+            'lazydir = lazydir.cli:cli',
         ],
     },
 )
