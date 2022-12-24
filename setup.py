d2adef691f8d3cd428a0263c4a446fa781461932
leogem2003 <loegem05@gmail.com>
2022-12-24 17:33:04 +0100

    changed version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lazydir',
-    version='0.1.0',
+    version='1.0.0',
     py_modules=find_packages(),
     install_requires=[
         'Click',
